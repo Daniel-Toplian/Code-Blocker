d0e66b64708bafae3b8e46673f95ed37dc29e1da
Daniel-Toplian <danieltoplian@gmail.com>
2024-01-28 23:11:20 +0200

    add timer to reset the isPass variable

--- background.js
+++ background.js
@@ -1,7 +1,60 @@
-let lastDate
+let isPassed = false
 let todaysQuestion
 let questionDifficulty = 'Random'
 const difficulties = ['Easy', 'Medium', 'Hard', 'Random']
+
+// event handlers
+// on installation
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('Code Blocker extension installed.')
+  chrome.alarms.create('resetVariable', { periodInMinutes: 24 * 60 })
+})
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === 'resetVariable') {
+    isPassed = false
+  }
+})
+
+// on every tab update
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (todaysQuestion && tab.url.includes(todaysQuestion.link)) {
+    return
+  }
+
+  if (changeInfo.status === 'loading') {
+    redirect()
+  }
+})
+
+// on difficulty change
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  let error = 'difficulty is null'
+  if (message.difficulty) {
+    if (difficulties.includes(message.difficulty)) {
+      questionDifficulty = message.difficulty
+      if (questionDifficulty == 'Random') {
+        getRandomQuestion()
+          .then(() => {
+            redirect()
+            sendResponse('Difficulty has been changed!')
+            return
+          })
+          .catch((error) => {
+            sendResponse('Error changing difficulty: ' + error)
+          })
+      }
+
+      redirect()
+      sendResponse('Difficulty has been changed!')
+      return
+    }
+
+    error = 'an unfamiliar difficulty was recieved'
+  }
+
+  sendResponse('Unable to change difficulty, error: ' + error)
+})
 
 // functions
 async function redirect() {
@@ -11,49 +64,45 @@ async function redirect() {
     let redirectUrl = result.link
     let tab = await getCurrentTab()
 
-    if (shouldRedirect(tab, redirectUrl)) {
-      redirectCurrentTab(tab, redirectUrl)
-    }
+    shouldRedirect(tab, redirectUrl)
+      .then((result) => {
+        console.log('shouldRedirect : ' + result)
+        if (result) {
+          redirectCurrentTab(tab, redirectUrl)
+        }
+      })
+      .catch((error) => {
+        console.error('Error while redirecting. Error:' + error)
+      })
   } catch (error) {
-    console.log(error)
+    console.error('Error while redirecting. Error:' + error)
   }
+}
 
-  async function redirectCurrentTab(currentTab, newUrl) {
-    try {
-      if (currentTab) {
-        chrome.tabs.update(currentTab.id, { url: newUrl })
-        return
-      }
-      console.log('No active tabs found.')
-    } catch (error) {
-      console.error(error)
+async function redirectCurrentTab(currentTab, newUrl) {
+  try {
+    if (currentTab) {
+      chrome.tabs.update(currentTab.id, { url: newUrl })
+      return
     }
+    console.log('No active tabs found.')
+  } catch (error) {
+    console.error(error)
   }
+}
 
-  async function refreshTab() {
-    getCurrentTab()
-      .then((tab) => {
-        const code = 'window.location.reload();'
-        chrome.tab.executeScript(tab.id, { code: code })
-      })
-      .catch((err) => {
-        console.log('unable to refresh the tab beacuase: ' + err)
-      })
-  }
-
-  async function getCurrentTab() {
-    return new Promise((resolve, reject) => {
-      chrome.windows.getCurrent((window) => {
-        chrome.tabs.query({ active: true, windowId: window.id }, (tabs) => {
-          if (tabs && tabs.length > 0) {
-            resolve(tabs[0])
-          } else {
-            reject(new Error('unable to get current tab'))
-          }
-        })
+async function getCurrentTab() {
+  return new Promise((resolve, reject) => {
+    chrome.windows.getCurrent((window) => {
+      chrome.tabs.query({ active: true, windowId: window.id }, (tabs) => {
+        if (tabs && tabs.length > 0) {
+          resolve(tabs[0])
+        } else {
+          reject(new Error('unable to get current tab'))
+        }
       })
     })
-  }
+  })
 }
 
 async function getTodaysQuestion() {
@@ -62,7 +111,6 @@ async function getTodaysQuestion() {
     (todaysQuestion.difficulty == questionDifficulty ||
       questionDifficulty == 'Random')
   ) {
-    console.log('return todaysQuestion')
     return todaysQuestion
   }
 
@@ -86,81 +134,52 @@ async function getRandomQuestion() {
     const questionsAmount = Object.keys(questions).length
 
     const randomIndex = Math.floor(Math.random() * questionsAmount)
-    const randomQuestion = questions[randomIndex]
+    // const randomQuestion = questions[randomIndex]
+    const randomQuestion = questions[6]
 
     todaysQuestion = randomQuestion
-    console.log(todaysQuestion)
     return todaysQuestion
   } catch (error) {
     throw error
   }
 }
 
-function shouldRedirect(currentTab, redirectUrl) {
+async function shouldRedirect(currentTab, redirectUrl) {
   const currentUrl = currentTab.url
-  const isPassed = isQuestionAnswerd()
-  return !currentUrl.includes(redirectUrl) && !isPassed
-}
-
-function isQuestionAnswerd() {
-  let isPassed = false
-  // chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-  //   chrome.tabs.executeScript(
-  //     tabs[0].id,
-  //     {
-  //       code: 'document.getElementsByClassName("text-body flex flex-none items-center gap-1 py-1.5 text-text-secondary dark:text-text-secondary").length > 0;',
-  //     },
-  //     function (result) {
-  //       isPassed = result[0]
-  //     }
-  //   )
-  // })
-
-  return isPassed
-}
-
-// event handlers
-
-// on installation
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('Code Blocker extension installed.')
-  redirect()
-})
-
-// on every tab update
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'loading') {
-    redirect()
-  }
-})
-
-// on difficulty change
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  let error = 'difficulty is null'
-  if (message.difficulty) {
-    if (difficulties.includes(message.difficulty)) {
-      questionDifficulty = message.difficulty
-      if (questionDifficulty == 'Random') {
-        getRandomQuestion()
-          .then(() => {
-            redirect()
-            sendResponse('Difficulty has been changed!')
-            refreshTab()
-            return
-          })
-          .catch((err) => {
-            sendResponse('Error changing difficulty: ' + err)
-          })
-      }
-
-      redirect()
-      sendResponse('Difficulty has been changed!')
-      refreshTab()
-      return
+  return new Promise((resolve, reject) => {
+    if (!currentUrl.includes(redirectUrl) || !isPassed) {
+      isQuestionAnswered(redirectUrl)
+        .then((result) => {
+          isPassed = result
+          resolve(!currentUrl.includes(redirectUrl) && !result)
+        })
+        .catch((error) => {
+          reject('Problem to detrmine is question is answered, error: ' + error)
+        })
     }
+  })
+}
 
-    error = 'an unfamiliar difficulty was recieved'
-  }
+async function isQuestionAnswered(url) {
+  return new Promise((resolve) => {
+    chrome.tabs.create({ url: url, active: false }, (tab) => {
+      chrome.scripting.executeScript(
+        { target: { tabId: tab.id }, function: getSolvedStatus },
+        async (results) => {
+          chrome.tabs.remove(tab.id)
+          resolve(results[0].result)
+        }
+      )
+    })
+  })
+}
 
-  sendResponse('Unable to change difficulty, error: ' + error)
-})
+// todo: this is still not working - fix this please
+function getSolvedStatus() {
+  // if there is an element, that means the question is solved
+  // document.getElementsByClassName(
+  //   'text-body flex flex-none items-center gap-1 py-1.5 text-text-secondary dark:text-text-secondary'
+  // ).length > 0
+
+  return true
+}
